995fd9fd9cf3b9c1216afba2918f45ef3bf624c1
guillermooo <none@none>
2010-08-25 21:47:46 +0200

    Added a few commands.
    --HG--
    branch : v0.1

--- actions.py
+++ actions.py
@@ -1,4 +1,9 @@
 import re
+import sublime
+
+
+def dispatch(cmd, *args):
+    CMDS["simple_cmds"][cmd](*args)
 
 def exclude(view, what):
     view.runCommand("splitSelectionIntoLines")
@@ -10,3 +15,35 @@ def replace(view, what, with_this):
     view.runCommand("splitSelectionIntoLines")
     for r in view.sel():
         view.replace(r, re.sub(what, with_this, view.substr(r)))
+
+
+def saveBuffer(view):
+    view.runCommand("save")
+    sublime.statusMessage("Saved %s" % view.fileName())
+
+def editFile(view):
+    view.window().runCommand("openFileInProject")
+
+def exitSublime(view):
+    view.window().runCommand("exit")
+
+def nextViewInStack(view):
+    view.window().runCommand("nextViewInStack")
+
+def previousViewInStack(view):
+    view.window().runCommand("prevViewInStack")
+
+def promptSelectFile(view):
+    view.window().runCommand("promptSelectFile")
+
+
+CMDS = {
+     "simple_cmds": {
+        "w": saveBuffer,
+        "e": editFile,
+        "q": exitSublime,
+        "n": nextViewInStack,
+        "N": previousViewInStack,
+        "ls": promptSelectFile,
+    },
+}
--- us_range_parser.py
+++ us_range_parser.py
@@ -3,6 +3,7 @@ import pprint
 
 opt_modif = p.Group(p.Optional(p.Word("+-") + p.Word(p.nums)))
 
+simple_cmd = p.Word("weqnN") | p.Literal("ls")
 nrng = p.Word(p.nums) + opt_modif
 hrng = p.Word(".$") + opt_modif
 fs = "/" + p.Word(p.alphas) + "/" + opt_modif
@@ -11,7 +12,7 @@ bs = "?" + p.Word(p.alphas) + "?" + opt_modif
 the_range = p.delimitedList(p.Group(hrng) | p.Group(fs) | p.Group(bs) | p.Group(nrng))
 cmd = p.Optional(p.Literal("-")) + p.Literal("sel") + "/" + p.CharsNotIn("/") + "/" + p.Group(p.Optional(p.Word("i")))
 cmd2 = p.Literal("rep") + "/" + p.CharsNotIn("/") + "/" + p.CharsNotIn("/") + "/" + p.Group(p.Optional(p.Word("i")))
-grammar = p.Group(p.Optional(the_range)).setResultsName("range") + p.Group(p.delimitedList(p.Group(p.Optional(cmd | cmd2)), delim=";")).setResultsName("cmds")
+grammar = simple_cmd.setResultsName("cmd") | p.Group(p.Optional(the_range)).setResultsName("range") + p.Group(p.delimitedList(p.Group(p.Optional(cmd | cmd2)), delim=";")).setResultsName("cmds")
 
 def parse(a):
     return grammar.parseString(a)
