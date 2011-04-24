b48e491dcdf07074a8ef677e487d5fc9242198be
guillermooo <guilan70@hotmail.com>
2011-04-24 16:31:03 +0200

    More style fixes.

--- vimactions.py
+++ vimactions.py
@@ -1,34 +1,44 @@
 import sublime
 import decorators
 
+
 def unknown_command(cmd):
     sublime.status_message("UBERSELECTION (vim) -- Command unknown: %s" % cmd)
 
-def saveBuffer(view, *args):
+
+def save_buffer(view, *args):
     cmd = "save" if not args else "save_as"
     view.window().run_command(cmd)
+
 
 def save_buffer_all(view):
     view.window().run_command("save_all")
 
+
 def save_buffer_and_exit(view):
-    saveBuffer(view)
-    exitSublime(view)
+    save_buffer(view)
+    exit_sublime(view)
+
 
 def edit_file(view):
     view.window().run_command("open_file_in_project")
 
+
 def exit_sublime(view):
     view.window().run_command("exit")
+
 
 def next_view_in_stack(view):
     view.window().run_command("next_view_in_stack")
 
+
 def previous_view_in_stack(view):
     view.window().run_command("prev_view_in_stack")
 
+
 def prompt_select_file(view):
     view.window().run_command("prompt_select_file")
+
 
 def dispatch(view, cmd, *args):
     try:
@@ -40,7 +50,7 @@ def dispatch(view, cmd, *args):
 CMDS = {
      "simple": {
         "wall": save_buffer_all,
-        "w": saveBuffer,
+        "w": save_buffer,
         "wq": save_buffer_and_exit,
         "ZZ": save_buffer_and_exit,
         "e": edit_file,
